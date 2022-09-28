e6d457b8fcae74baf5036117776219b45fd58f46
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-09-29 01:34:46 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,9 +6,6 @@ from pyrogram import Client, errors
 # load config
 config = (json.load(open("config.json")))
 # group target link
-group_source_id = str(config['group_target_username'])
-group_source_id = str(config['group_source_username'])
-#workdir = 'session/'
 
 
 def main():
@@ -20,11 +17,6 @@ def main():
         with Client(phone, api_id, api_hash, workdir="session") as app:
             if app.get_me():
                 print(phone, "is logined")
-                try:
-                    app.join_chat(group_target_id)
-                    app.join_chat(group_source_id)
-                except BaseException:
-                    print("couldn't add u to the groups or maybe this number already in group")
             else:
                print(phone, "login failed")
 main()
